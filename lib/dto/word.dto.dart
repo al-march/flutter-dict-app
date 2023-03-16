65e436b71637fd901101188a4e51b73c4d02ac98
Alexander <valverin95@gmail.com>
2023-03-16 10:20:33 +0300

    init app

---
+++ lib/dto/word.dto.dart
@@ -0,0 +1,13 @@
+class Word {
+  final String name;
+  final String type;
+  final String difficult;
+  final String audio;
+
+  Word({
+    required this.name,
+    required this.type,
+    required this.difficult,
+    required this.audio,
+  });
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        theme: ThemeData.from(
+          useMaterial3: true,
+          colorScheme: const ColorScheme.dark().copyWith(
+            background: const Color.fromARGB(255, 25, 25, 25),
+            primary: Colors.teal,
+            surface: const Color.fromARGB(255, 40, 40, 40),
+          ),
+        ),
+        home: const Text("Hello world"),
+      ),
+    );
+  }
+}
+
+class AppState extends ChangeNotifier {}
