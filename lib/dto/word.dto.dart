935ad98654ae07e07c90edbad6436acd49bf0255
Alexander <valverin95@gmail.com>
2023-03-21 23:06:07 +0300

    word dto

--- lib/dto/word.dto.dart
+++ lib/dto/word.dto.dart
@@ -10,4 +10,18 @@ class Word {
     required this.difficult,
     required this.audio,
   });
+
+  factory Word.fromJson(Map json) => Word(
+        name: json['name'],
+        type: json['type'],
+        difficult: json['difficult'],
+        audio: json['audio'],
+      );
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'type': type,
+    'difficult': difficult,
+    'audio': audio,
+  };
 }
