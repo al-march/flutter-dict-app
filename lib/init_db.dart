5983d3f71a6ed38a6b2e8bcaf1145b3759a0fa62
Alexander <valverin95@gmail.com>
2023-03-21 23:05:52 +0300

    init db

---
+++ lib/init_db.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/services.dart';
+import 'package:mobile/dto/word.dto.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
+import 'dart:io' as io;
+
+class DictDB {
+  late Database _db;
+
+  Future<void> init() async {
+    io.Directory applicationDirectory =
+        await getApplicationDocumentsDirectory();
+
+    String dbPathEnglish =
+        path.join(applicationDirectory.path, "englishDictionary.db");
+
+    bool dbExistsEnglish = await io.File(dbPathEnglish).exists();
+
+    if (!dbExistsEnglish) {
+      // Copy from asset
+      ByteData data = await rootBundle.load(path.join("db", "dict.db"));
+      List<int> bytes =
+          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+      // Write and flush the bytes written
+      await io.File(dbPathEnglish).writeAsBytes(bytes, flush: true);
+    }
+
+    this._db = await openDatabase(dbPathEnglish);
+  }
+
+  /// get all the words from english dictionary
+  Future<List<Word>> getAllWords() async {
+    if (_db == null) {
+      throw "bd is not initiated, initiate using [init(db)] function";
+    }
+    List<Map> words = [];
+
+    await _db.transaction((txn) async {
+      words = await txn.query(
+        "words",
+      );
+    });
+
+    return words.map((e) => Word.fromJson(e)).toList();
+  }
+
+  Future<List<Word>> getSlice(int limit) async {
+    List<Map> words = [];
+
+    await _db.transaction((txn) async {
+      words = await txn.query(
+        "words",
+        limit: limit,
+      );
+    });
+
+    return words.map((e) => Word.fromJson(e)).toList();
+  }
+}
